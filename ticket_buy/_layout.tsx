85e04fb20f3f208ec1aad5ae3dc3065646ff86da
Gabriel Dossa <gabrielvinces2@gmail.com>
2025-10-01 15:48:41 -0300

    Fixing some archive.

---
+++ ticket_buy/_layout.tsx
@@ -0,0 +1,33 @@
+// app/_layout.tsx
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        {/* Tela principal */}
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            title: 'Ifood',
+          }}
+        />
+        {/* Modal (se precisar no futuro) */}
+        <Stack.Screen
+          name="modal"
+          options={{
+            presentation: 'modal',
+            title: 'Modal',
+          }}
+        />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
